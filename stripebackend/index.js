a226de651ca6dbd5c33f8100a986242486990f2e
Suvojit Ray <suvojit.royuvo@gmail.com>
2021-05-11 01:01:10 +0530

    payment_success

---
+++ stripebackend/index.js
@@ -0,0 +1,43 @@
+const cors=require("cors")
+const express=require('express')
+
+
+const stripe=require("stripe")("sk_test_51IpczwSH2HRN2FzsEEXovr4WwlrLuXfWHvIExQwvAdENcM1Q3pD5qzJxvbshMvNoGHCIy6xuWukTsIIsp6uSenPI000seyevZz")
+const uuid=require("uuid")
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/",(req,res)=>{
+    res.send("This works")
+})
+
+app.post('/payment',(req,res)=>{
+    const {product,token}=req.body;
+    console.log("PRODUCT",product);
+    console.log("PRICE",product.price);
+    const idempotencyKey= uuid()
+
+    return stripe.customers.create({
+        email:token.email,
+        source: token.id
+    }).then(customer=>{
+        stripe.charges.create({
+            amount:product.price*100,
+            currency:'usd',
+            customer:customer.id,
+            receipt_email:token.email,
+            description:product.name
+        },{idempotencyKey})
+    })
+    .then(result=> res.status(200).json(result))
+    .catch(err=>console.log(err))
+})
+
+app.listen(4000,()=>{
+    console.log("Listening at port 4000")
+})
